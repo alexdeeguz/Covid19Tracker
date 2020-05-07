dec0d3ee3b51bb4a390c1e92a2ace5fcbbbf2359
alex <alexdg316@gmail.com>
2020-05-07 02:34:14 -0700

    Create cards in cards component

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
+import { Card, CardContent, Typography, Grid } from '@material-ui/core'
+import styles from './cards.module.css'
+const Cards = (props) => {
+    
+    return (
+        <div className="styles.container">
+            <Grid container spacing={3} justify="center">
 
-class Cards extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>CARDS</h1>
-            </div>
-        )
-    }
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary" >REAL DATE</Typography>
+                        <Typography variant="body2">Number of active cases of Covid-19</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary" >REAL DATE</Typography>
+                        <Typography variant="body2">Number of recoveries of Covid-19</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary" >REAL DATE</Typography>
+                        <Typography variant="body2">Number of deaths caused by Covid-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
+    )
 }
 
 export default Cards
